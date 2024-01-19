cd1b39c65028c3096fa4b754d4bd0b26095dcc3c
owolabi-develop <owolabidevelop84@gmail.com>
2024-01-19 12:18:35 -0800

    add lambda stack

---
+++ lambda/sales_data_producer.py
@@ -0,0 +1,40 @@
+import json
+import boto3
+import os
+import random
+from faker import Faker
+from decimal import Decimal
+
+
+fake = Faker()
+
+def generate_property():
+   
+    """Generate a fake real estate property."""
+    
+   
+
+    property_details = {  }
+    return property_details
+
+def generate_properties(num_properties):
+    """Generate multiple fake real sales data ."""
+    properties = [generate_property() for _ in range(num_properties)]
+    return properties
+
+def handler(event,context):
+    
+    kinesis_client = boto3.client('kinesis')
+    
+    properties_data = generate_properties(200)
+    
+    for properties in properties_data:
+        response = kinesis_client.put_record(
+            StreamName=os.environ['STREAM_NAME'],
+            Data=json.dumps(properties).encode('utf-8'),
+            PartitionKey=properties['city']
+        )
+    return response
+    
+    
+    
---
+++ lambdaStack/lambda_producer_stack.py
@@ -0,0 +1,50 @@
+from constructs import Construct
+import aws_cdk as cdk
+from  aws_cdk import (
+    Stack,
+    Duration,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+)
+
+ENVIRONMENT = {
+    "STREAM_NAME":"salesData"
+}
+
+
+class HomePropertiesStack(Stack):
+     def __init__(self, scope: Construct, construct_id: str, **Kwargs):
+        super().__init__(scope, construct_id, **Kwargs)
+        
+        lambda_role = _iam.Role(
+            self,
+            "lambdaRole",
+             assumed_by=_iam.ServicePrincipal("lambda.amazonaws.com"),
+             managed_policies=[
+                 _iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+                 _iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchFullAccess"),
+                 _iam.ManagedPolicy.from_aws_managed_policy_name("AmazonKinesisFullAccess"),
+                
+             ]
+        )
+        
+        sales_data_layer = _lambda.LayerVersion(
+            self,
+            "salesdatalayer",
+            code=_lambda.AssetCode("layer/sale_layer")
+        )
+        
+        
+        
+        lambda_sales_data_producer = _lambda.Function(self,
+                                             "propertiesdataproducer",
+                                             runtime=_lambda.Runtime.PYTHON_3_12,
+                                             code=_lambda.Code.from_asset("lambda"),
+                                             handler="sales_data_producer.handler",
+                                             timeout=Duration.minutes(3),
+                                             layers=[sales_data_layer],
+                                             role=lambda_role,
+                                             environment=ENVIRONMENT
+                                             )
+        
+     
